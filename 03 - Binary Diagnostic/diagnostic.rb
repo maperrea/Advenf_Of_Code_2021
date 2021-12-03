d401123d5cb02f9308d1eed25fa2e3f42ce7c95e
maperrea <maperrea@student.s19.be>
2021-12-03 17:04:40 +0100

    03 cleanup

--- 03 - Binary Diagnostic/diagnostic.rb
+++ 03 - Binary Diagnostic/diagnostic.rb
@@ -33,52 +33,12 @@ def get_most_common(input, i)
 		else
 			zero += 1 end
 	end
-#	puts ">>>"
-#	puts one
-#	puts zero
-#	puts "<<<"
 	if (one >= zero)
 		return (1 << i)
 	else
 		return 0
 	end
 end
-
-=begin this is shit?? but why
-oxygen = input.clone
-co2 = input.clone
-
-11.downto(0) do |i|
-	gamma = get_most_common(oxygen, i)
-	j = 0
-	puts gamma.to_s(2)
-	for j in (0...oxygen.size)
-		if ((oxygen[j] & (1 << i)) == (~gamma) & (1 << i)) && oxygen.size > 1 
-			oxygen.delete_at(j)
-		j += 1
-		end
-	end
-	puts oxygen.size
-	j = 0
-	for j in (0...co2.size)
-		if ((co2[j] & (1 << i)) == gamma & (1 << i)) && co2.size > 1
-			co2.delete_at(j)
-		else
-			j += 1
-		end
-	end
-end
-
-puts "___"
-puts oxygen.size
-puts co2.size
-puts oxygen[0]
-puts co2[0]
-puts oxygen[0] * co2[0]
-*/
-=end
-
-## Other way ##
 
 oxygen = input.clone
 co2 = input.clone
@@ -88,15 +48,10 @@ co2 = input.clone
 	if oxygen.size > 1
 		oxygen.delete_if { |j| j & (1 << i) == ~most_common & (1 << i) }
 	end
-#	puts oxygen.size
 	most_common = get_most_common(co2, i)
 	if co2.size > 1
 		co2.delete_if { |j| j & (1 << i) == most_common & (1 << i) }
 	end
-#	puts co2.size
-#	puts "______"
 end
 
-#puts oxygen[0]
-#puts co2[0]
 puts oxygen[0] * co2[0]
