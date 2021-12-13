d258921cdd95416c497d4d560ab0e4d8b1d0f872
Perreaux Matthis <matperthis@gmail.com>
2021-12-13 12:18:17 +0100

    15 done

--- 15 - Chiton/path.rb
+++ 15 - Chiton/path.rb
@@ -2,27 +2,27 @@
 
 ## Part One ##
 
-W = @input.size
-H = @input[0].size
-@map = @input
+#W = @input.size
+#H = @input[0].size
+#@map = @input
 
 ## Part Two ##
 
-#IW = @input.size
-#IH = @input[0].size
-#
-#W = IW * 5
-#H = IH * 5
-#
-#@map = Array.new(W) { Array.new(H) }
-#(0...W).each do |x|
-#	(0...H).each do |y|
-#		@map[x][y] = @input[x % IW][y % IH] + (x / IW) + (y / IH)
-#		if (@map[x][y] > 9)
-#			@map[x][y] = (@map[x][y] % 10) + 1
-#		end
-#	end
-#end
+IW = @input.size
+IH = @input[0].size
+
+W = IW * 5
+H = IH * 5
+
+@map = Array.new(W) { Array.new(H) }
+(0...W).each do |x|
+	(0...H).each do |y|
+		@map[x][y] = @input[x % IW][y % IH] + (x / IW) + (y / IH)
+		if (@map[x][y] > 9)
+			@map[x][y] = (@map[x][y] % 10) + 1
+		end
+	end
+end
 
 class Node
 	attr_reader :x
@@ -43,7 +43,7 @@ class Node
 	end	
 end
 
-@closed_list = Array.new
+@closed_list = Array.new(W) { Array.new(H) { -1 } }
 @open_list = Array.new
 
 @open_list << Node.new(0, 0, 0)
@@ -61,7 +61,7 @@ def print_explored(closed, open)
 	(0...W).each do |x|
 		(0...H).each do |y|
 			n = Node.new(x, y)
-			if (found = find_node(closed, n))
+			if (closed[x][y] != -1)
 				print " # "
 			elsif (found = find_node(open, n))
 				print "%3d" % [found.dist.to_s]
@@ -73,76 +73,65 @@ def print_explored(closed, open)
 	end
 end
 
-step = 0
+#step = 0
 while !@open_list.empty? do
-	step += 1
-	@open_list.sort_by! { |node| node.dist }
+#	step += 1
+#	puts step
 	n = @open_list.shift
 	if n.x == W - 1 && n.y == H - 1
 		@final = n
 		break
 	end
-	(n.x - 1 .. n.x + 1).each do |x|
+	[n.x - 1, n.x + 1].each do |x|
 		if x != n.x && x >= 0 && x < W
 			new = Node.new(x, n.y, n.cost + @map[x][n.y])
-			if (found = find_node(@closed_list, new)) || (found = find_node(@open_list, new))
+			if @closed_list[x][n.y] != -1
+				if @closed_list[x][n.y] > new.cost
+					@closed_list[x][n.y] = new.cost
+				end
+			elsif (found = find_node(@open_list, new))
 				if found.cost > new.cost
 					found.set_cost(new.cost)
 				end
 			else
-				@open_list << new
+				if (index = @open_list.index { |e| e.cost >= new.cost })
+					@open_list.insert(index, new)
+				else
+					@open_list << new
+				end
 			end
 		end
 	end
-	(n.y - 1 .. n.y + 1).each do |y|
+	[n.y - 1, n.y + 1].each do |y|
 		if y != n.y && y >= 0 && y < H
 			new = Node.new(n.x, y, n.cost + @map[n.x][y])
-			if (found = find_node(@closed_list, new)) || (found = find_node(@open_list, new))
+			if @closed_list[n.x][y] != -1
+				if @closed_list[n.x][y] > new.cost
+					@closed_list[n.x][y] = new.cost
+				end
+			elsif (found = find_node(@open_list, new))
 				if found.cost > new.cost
 					found.set_cost(new.cost)
 				end
 			else
-				@open_list << new
+				if (index = @open_list.index { |e| e.cost >= new.cost })
+					@open_list.insert(index, new)
+				else
+					@open_list << new
+				end
 			end
 		end
 	end
-	@closed_list << n
+	@closed_list[n.x][n.y] = n.cost
 #	puts "[%d, %d] : %d" % [n.y, n.x, n.cost]
 #	print_explored(@closed_list, @open_list)
 #	puts "__________"
 end
 
-puts step
-puts @final.cost
+#puts step
 
-def print_path()
-	@path = [@final]
-	previous = @final
-	while previous.x != 0 || previous.y != 0
-		min = nil
-		(previous.x - 1 .. previous.x + 1).each do |x|
-			if x != previous.x && x >= 0 && x < H
-				if (found = find_node(@closed_list, Node.new(x, previous.y, 0)))
-					if !min || found.cost < min.cost
-						min = found
-					end
-				end
-			end
-		end
-		(previous.y - 1 .. previous.y + 1).each do |y|
-			if y != previous.y && y >= 0 && y < W
-				if (found = find_node(@closed_list, Node.new(previous.x, y, 0)))
-					if !min || found.cost < min.cost
-						min = found
-					end
-				end
-			end
-		end
-		previous = min
-		@path.unshift(previous)
-	end
-	
-	@path.each do |node|
-		puts "[%d, %d] : %d" % [node.y, node.x, node.cost]
-	end
+if !@final
+	puts "Error: no end found"
+else
+	puts @final.cost
 end
